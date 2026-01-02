3692b9bf5f8302599b291ae8650dcd30621a5ba5
Flow2510 <sendra.florian@gmail.com>
2026-01-02 22:38:59 +0100

    ajout d'un tri sur le shop

--- src/components/shop/shop.jsx
+++ src/components/shop/shop.jsx
@@ -1,20 +1,77 @@
 import { NavLink } from 'react-router-dom';
 import './shop.scss';
 import Loading from '../loading/loading';
+import { useMemo, useState } from 'react';
 
 export default function Shop({ items }) {
+    const [slice, setSlice] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [sort, setSort] = useState("") 
+
+    const sortedItems = useMemo(() => {
+        if (!items) return <Loading />
+
+        switch (sort) {
+            case "date":
+                return [...items].sort(
+                    (a, b) => new Date(b.created_at) - new Date(a.created_at)
+                );
+
+            case "reverse-date":
+                return [...items].sort(
+                    (a, b) => new Date(a.created_at) - new Date(b.created_at)
+                );
+
+            case "name":
+                return [...items].sort(
+                    (a, b) => a.name.localeCompare(b.name, 'fr', { sensitivity: 'base' })
+                );
+
+            case "reverse-name":
+                return [...items].sort(
+                    (a, b) => b.name.localeCompare(a.name, 'fr', { sensitivity: 'base' })
+                );
+
+            default:
+                return items;
+        }
+    }, [items, sort]);
+
     if (!items) return <Loading />
+
+    const totalPage = Math.ceil(sortedItems.length / 3);
+    const handleNextClick = () => {
+        setSlice(prev => prev  + 3)
+        setCurrentPage(prev => prev + 1)
+    };
+    const handleBackClick = () => {
+        if (slice !== 0) {
+            setSlice(prev => prev - 3)
+            setCurrentPage(prev => prev - 1)
+        }
+    };
 
     return(
         <section className='shop'>
             <h2>La boutique</h2>
             <div className='shop__menu'>
-
+                <select name="" id="" onChange={(e) => setSort(e.target.value)}>
+                    <option value="">Tri</option>
+                    <option value="name">Nom A-Z</option>
+                    <option value="reverse-name">Nom Z-A</option>
+                    <option value="date">Plus récent</option>
+                    <option value="reverse-date">Plus ancien</option>
+                </select>
             </div>
             <div className='shop__gallery'>
-                {items.map((item, index) => (
+                {sortedItems.slice(slice, slice + 3).map((item, index) => (
                     <NavLink to={`/${item.name}`} className='shop__gallery-link' key={item.id + index}><img className='shop__gallery-image' src={item.image} alt="" /></NavLink>
                 ))}
+            </div>
+            <div className='shop__index'>
+                <button disabled={slice === 0} onClick={handleBackClick}><i className="fa-solid fa-chevron-left"></i></button>
+                <p>{currentPage} sur {totalPage}</p>
+                <button disabled={currentPage === totalPage} onClick={handleNextClick}><i className="fa-solid fa-chevron-right"></i></button>
             </div>
         </section>
     )
